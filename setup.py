860eaee807b7df37e5594bda3478355da3f2539d
Simeon Warner <simeon.warner@cornell.edu>
2018-08-07 13:38:56 -0400

    Remove 3.3 claim, add 3.6

--- setup.py
+++ setup.py
@@ -50,9 +50,9 @@ setup(
                  "Operating System :: OS Independent",  # is this true? know Linux & OS X ok
                  "Programming Language :: Python",
                  "Programming Language :: Python :: 2.7",
-                 "Programming Language :: Python :: 3.3",
                  "Programming Language :: Python :: 3.4",
                  "Programming Language :: Python :: 3.5",
+                 "Programming Language :: Python :: 3.6",
                  "Topic :: Software Development :: Libraries :: Python Modules"],
     author='Simeon Warner',
     author_email='simeon.warner@cornell.edu',
